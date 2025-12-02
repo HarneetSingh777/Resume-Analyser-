1d6e0efdd0d8c5fd26e282d4245ed47b49eb34b9
HarneetSingh777 <harneet.chandhyoke@gmail.com>
2025-12-02 14:00:04 +0530

    Update trial2.py

--- trial2.py
+++ trial2.py
@@ -20,7 +20,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=GEMINI_API_KEY)
 
 # Gemini model
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 # ---------------------------
 # Modern CSS Styling
